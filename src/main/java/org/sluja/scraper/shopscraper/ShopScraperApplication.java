5f8d8080d8ff64e6d288766c2addc4f34f6893d2
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-10-23 23:35:54 +0200

    Init commit

---
+++ src/main/java/org/sluja/scraper/shopscraper/ShopScraperApplication.java
@@ -0,0 +1,13 @@
+package org.sluja.scraper.shopscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sluja/scraper/shopscraper/ShopScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sluja.scraper.shopscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
