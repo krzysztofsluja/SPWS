78acdaf05eb41dad7f4f15452f64fbba80fb9274
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-10-26 22:56:53 +0200

    #3 - refactor - encapsulate the code in one class which can be entered only by url variable

--- src/main/java/org/sluja/scraper/shopscraper/connector/service/crawler/WebsiteCrawlerConnector.java
+++ src/main/java/org/sluja/scraper/shopscraper/connector/service/crawler/WebsiteCrawlerConnector.java
@@ -2,20 +2,46 @@ package org.sluja.scraper.shopscraper.connector.service.crawler;
 
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
-import org.sluja.scraper.shopscraper.connector.dtos.request.ConnectRequest;
 import org.sluja.scraper.shopscraper.connector.dtos.request.crawler.CrawlerConnectRequest;
 import org.sluja.scraper.shopscraper.connector.exceptions.connection.WebsiteConnectionTimeoutException;
+import org.sluja.scraper.shopscraper.connector.exceptions.request.IncorrectConnectionRequestStructureException;
 import org.sluja.scraper.shopscraper.connector.interfaces.IConnector;
+import org.sluja.scraper.shopscraper.connector.interfaces.IWebsiteConnector;
 
-public class WebsiteCrawlerConnector implements IConnector<Void, CrawlerConnectRequest> {
-    @Override
-    public Void connect(final CrawlerConnectRequest request) throws WebsiteConnectionTimeoutException {
-        try(request){
-            request.getDriver().get(request.getUrl());
-        } catch (final TimeoutException _) {
-            throw new WebsiteConnectionTimeoutException();
-        }
-        return null;
+public class WebsiteCrawlerConnector implements IWebsiteConnector<WebDriver> {
+
+    private final IConnector<Void, CrawlerConnectRequest> crawlerConnector;
+
+    public WebsiteCrawlerConnector() {
+        crawlerConnector = new CrawlerConnector();
     }
 
+    @Override
+    public WebDriver getWebpage(String url) {
+        try(final CrawlerConnectRequest request = new CrawlerConnectRequest(url)) {
+            crawlerConnector.connect(request);
+            return request.getDriver();
+        } catch (final IncorrectConnectionRequestStructureException ex) {
+            //TODO ex
+            //TODO log
+            return null;
+        } catch (WebsiteConnectionTimeoutException e) {
+            //TODO ex
+            //TODO log
+            return null;
+        }
+    }
+
+    static class CrawlerConnector implements IConnector<Void, CrawlerConnectRequest> {
+        @Override
+        public Void connect(final CrawlerConnectRequest request) throws WebsiteConnectionTimeoutException {
+            try(request){
+                request.getDriver().get(request.getUrl());
+            } catch (final TimeoutException _) {
+                throw new WebsiteConnectionTimeoutException();
+            }
+            return null;
+        }
+
+    }
 }
