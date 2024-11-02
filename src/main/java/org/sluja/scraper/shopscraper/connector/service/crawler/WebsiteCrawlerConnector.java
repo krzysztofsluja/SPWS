76f620772452c2b8e1a50e9df880ce4fee89a188
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-11-02 10:44:40 +0100

    #6 - refactoring - add necessary annotations to WebsiteCrawlerConnector

--- src/main/java/org/sluja/scraper/shopscraper/connector/service/crawler/WebsiteCrawlerConnector.java
+++ src/main/java/org/sluja/scraper/shopscraper/connector/service/crawler/WebsiteCrawlerConnector.java
@@ -1,5 +1,6 @@
 package org.sluja.scraper.shopscraper.connector.service.crawler;
 
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.sluja.scraper.shopscraper.connector.dtos.request.crawler.CrawlerConnectRequest;
@@ -7,31 +8,32 @@ import org.sluja.scraper.shopscraper.connector.exceptions.connection.WebsiteConn
 import org.sluja.scraper.shopscraper.connector.exceptions.request.IncorrectConnectionRequestStructureException;
 import org.sluja.scraper.shopscraper.connector.interfaces.IConnector;
 import org.sluja.scraper.shopscraper.connector.interfaces.IWebsiteConnector;
+import org.sluja.scraper.shopscraper.exceptions.ExceptionWithErrorAndMessageCode;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class WebsiteCrawlerConnector implements IWebsiteConnector<WebDriver> {
 
     private final IConnector<Void, CrawlerConnectRequest> crawlerConnector;
 
-    public WebsiteCrawlerConnector() {
-        crawlerConnector = new CrawlerConnector();
-    }
-
     @Override
-    public WebDriver getWebpage(String url) {
+    public WebDriver getWebpage(String url) throws ExceptionWithErrorAndMessageCode {
         try(final CrawlerConnectRequest request = new CrawlerConnectRequest(url)) {
             crawlerConnector.connect(request);
             return request.getDriver();
         } catch (final IncorrectConnectionRequestStructureException ex) {
             //TODO ex
             //TODO log
-            return null;
+            throw ex;
         } catch (WebsiteConnectionTimeoutException e) {
             //TODO ex
             //TODO log
-            return null;
+            throw e;
         }
     }
 
+    @Component
     static class CrawlerConnector implements IConnector<Void, CrawlerConnectRequest> {
         @Override
         public Void connect(final CrawlerConnectRequest request) throws WebsiteConnectionTimeoutException {
