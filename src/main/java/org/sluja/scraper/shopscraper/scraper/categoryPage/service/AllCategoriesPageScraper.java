2d63062a4c2b8f42b4146adb0a68528a2b90ec07
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-11-02 12:08:25 +0100

    #6 - refactoring - implement changes to AllCategoriesPageScraper

--- src/main/java/org/sluja/scraper/shopscraper/scraper/categoryPage/service/AllCategoriesPageScraper.java
+++ src/main/java/org/sluja/scraper/shopscraper/scraper/categoryPage/service/AllCategoriesPageScraper.java
@@ -2,56 +2,49 @@ package org.sluja.scraper.shopscraper.scraper.categoryPage.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.sluja.scraper.shopscraper.connector.interfaces.IWebsiteConnector;
 import org.sluja.scraper.shopscraper.exceptions.ExceptionWithErrorAndMessageCode;
 import org.sluja.scraper.shopscraper.scraper.categoryPage.dtos.AllCategoriesPageRequest;
 import org.sluja.scraper.shopscraper.scraper.categoryPage.exceptions.EmptyCategoryPageElementsListException;
 import org.sluja.scraper.shopscraper.scraper.categoryPage.interfaces.ICategoryPageScraper;
-import org.sluja.scraper.shopscraper.scraper.service.AbstractBaseScraper;
+import org.sluja.scraper.shopscraper.scraper.categoryPage.interfaces.IPageExtractor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-@RequiredArgsConstructor
-@Component
 @Slf4j
-public class AllCategoriesPageScraper extends AbstractBaseScraper implements ICategoryPageScraper<String, AllCategoriesPageRequest> {
+@Component
+@RequiredArgsConstructor
+public class AllCategoriesPageScraper implements 
+        ICategoryPageScraper<String, AllCategoriesPageRequest>,
+        IPageExtractor<String, AllCategoriesPageRequest> {
 
     private final IWebsiteConnector<Document> websiteScraperConnector;
+    private final CategoryUrlProcessor urlProcessor;
 
     @Override
-    public List<String> getPages(final AllCategoriesPageRequest request) throws ExceptionWithErrorAndMessageCode {
-        final Document document = websiteScraperConnector.getWebpage(request.mainPageUrl());
-        final List<String> allCategoriesPageUrls = extractPages(document, request);
-        if (CollectionUtils.isEmpty(allCategoriesPageUrls)) {
-            log.warn("No category page URLs found for request: {}", request);
-            throw new EmptyCategoryPageElementsListException();
+    public Set<String> getPages(final AllCategoriesPageRequest request)
+            throws ExceptionWithErrorAndMessageCode {
+        log.debug("Starting to fetch pages for request: {}", request);
+        try {
+            final Document document = websiteScraperConnector.getWebpage(request.mainPageUrl());
+            return extractPages(document, request);
+        } catch (EmptyCategoryPageElementsListException e) {
+            log.error("No pages found for request: {}", request);
+            throw e;
         }
-        return allCategoriesPageUrls;
     }
 
     @Override
-    public List<String> extractPages(final Document document, final AllCategoriesPageRequest request) throws ExceptionWithErrorAndMessageCode {
-        return request.allCategoriesPageAttribute()
-                .stream()
-                .map(property -> {
-                    try {
-                        final Elements elements = this.scrape(document, property);
-                        return elements.eachAttr(request.pageUrlExtractAttribute());
-                    } catch (final ExceptionWithErrorAndMessageCode e) {
-                        log.error("Error extracting pages with property: {}", property, e);
-                        return new ArrayList<String>();
-                    }
-                })
-                .flatMap(List::stream)
-                .map(element -> element.replaceAll("\\s", StringUtils.EMPTY))
-                .distinct()
-                .filter(StringUtils::isNotEmpty)
-                .toList();
+    public Set<String> extractPages(final Document document,
+                                    final AllCategoriesPageRequest request)
+            throws EmptyCategoryPageElementsListException {
+        return urlProcessor.processUrls(
+                request.allCategoriesPageAttribute(),
+                document,
+                request.pageUrlExtractAttribute()
+        );
     }
 }
